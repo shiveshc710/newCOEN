0148898ac292310cba879715a8f3ecb4e85f5b95
Shivesh Chaudhary <126916276+shiveshc710@users.noreply.github.com>
2023-05-29 03:16:26 -0400

    Update CONFIG.java

--- src/main/java/com/example/coen_mp_concordiatravelwebapplication/config/CONFIG.java
+++ src/main/java/com/example/coen_mp_concordiatravelwebapplication/config/CONFIG.java
@@ -2,7 +2,7 @@ package com.example.coen_mp_concordiatravelwebapplication.config;
 
 public class CONFIG {
 
-      public static final String SQLURL = System.getenv("AZURE_SQL_CONNECTION_STRING");
+    public static final String SQLURL = System.getenv("CONNECTION_STRING");
 
     public static final String SQLUSER = "traveladmin@travelsystemserver";
 
